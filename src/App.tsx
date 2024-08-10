a1557978e8ae557b84ec9257ff25c75c4bef6efb
itayben-ami <itay.benami@finout.io>
2024-08-10 16:55:20 +0300

    Added supabase integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,21 @@
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-} from "@/components/ui/command"
 import './App.css'
 import { ThemeProvider } from "../components/ui/theme-provider"
-import { groupBy } from 'lodash'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+import WebsitesListPage from './pages/WebsitesListPage'
+
+
+const queryClient = new QueryClient()
 
 function App() {
-
-  const websites = [
-    {
-      name: "Tailwind",
-      url: "https://tailwindcss.com/",
-      icon: "https://tailwindcss.com/favicons/favicon.ico?v=3",
-      category: "Development"
-    },
-    {
-      name: "Github",
-      url: "https://github.com/finout-io/finout-application",
-      icon: "https://github.githubassets.com/favicons/favicon-dark.svg",
-      category: "Development"
-    },
-    {
-      name: "Figma",
-      url: "https://www.figma.com/files/team/996753143204589606/project/95312930?fuid=1361759885931930407",
-      icon: "https://static.figma.com/app/icon/1/favicon.svg",
-      category: "Work"
-    }
-  ];
-
-  const websitesByCategory = groupBy(websites, "category")
-
-
   return (
-    <ThemeProvider>
-
-      <div className="flex justify-center items-center">
-        <Command className="rounded-lg border shadow-md w-96">
-          <CommandInput placeholder="Search..." />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            {
-              Object.keys(websitesByCategory).map((categoryName, index) => (
-                <div>
-                  <CommandGroup heading={categoryName}>
-                    {
-                      websitesByCategory[categoryName].map((website) => (
-                        <CommandItem>
-                          <div className="flex gap-2 size-full items-center cursor-pointer"
-                            onClick={() => { window.open(website.url, "_blank") }}>
-                            <img src={website.icon} className="size-4" />
-                            <span>{website.name}</span>
-                          </div>
-                        </CommandItem>
-                      ))
-                    }
-                  </CommandGroup>
-                  {index < Object.keys(websitesByCategory).length - 1 && <CommandSeparator />}
-                </div>
-              ))
-            }
-          </CommandList>
-        </Command>
-      </div>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <WebsitesListPage />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/clients/supabase/index.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const SUPABASE_URL = "https://fgtfbeukcedamtskstqh.supabase.co/rest/v1"
+const SUPABASE_CLIENT_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZndGZiZXVrY2VkYW10c2tzdHFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMyODg2MjAsImV4cCI6MjAzODg2NDYyMH0.UQ8TZgJ1gx0V3f3JEiQmp8V9E3fxlcx5sbhW-4H7L2U"
+
+axios.defaults.headers.common = {
+    apikey: SUPABASE_CLIENT_ANON_KEY,
+    Authorization: `Bearer ${SUPABASE_CLIENT_ANON_KEY}`
+}
+
+export const getWebsites = async (select: string = "*") => {
+    const response = await axios.get(`${SUPABASE_URL}/websites`, {
+        params: {
+            select
+        }
+    });
+
+    return response.data;
+}
+
