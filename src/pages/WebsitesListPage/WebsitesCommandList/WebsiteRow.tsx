7c360e6463109f9867c1dda1a43ddbde6f75ef97
itayben-ami <itay.benami@finout.io>
2024-11-13 10:43:28 +0200

    Added enter support

--- src/pages/WebsitesListPage/WebsitesCommandList/WebsiteRow.tsx
+++ src/pages/WebsitesListPage/WebsitesCommandList/WebsiteRow.tsx
@@ -4,13 +4,13 @@ import Favicon from '../Favicon';
 
 export default function WebsiteRow({ website }: { website: Website }) {
   return (
-    <CommandItem className="h-11">
-      <div
-        className="flex gap-3 size-full items-center cursor-pointer"
-        onClick={() => {
-          window.location.href = website.url;
-        }}
-      >
+    <CommandItem
+      className="h-11"
+      onSelect={() => {
+        window.location.href = website.url;
+      }}
+    >
+      <div className="flex gap-3 size-full items-center cursor-pointer">
         <Favicon website={website} withAnimation={false} />
         <span className="text-md">{website.name}</span>
       </div>
